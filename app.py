5e9835b064398202dc31d6901c39955c20f00adf
kamran Arshad <ikramhashmi1111@gmail.com>
2025-02-19 11:08:43 +0500

    Flask Added

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template,request
 
 app =Flask(__name__)
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/",methods=['GET','POST'])
+def Home():
+    if request.method == 'POST':
+        
+        return render_template('index.html')
+
+    return render_template("index.html")    
+if __name__ == "__main__":
+    app.run(debug=True)    
