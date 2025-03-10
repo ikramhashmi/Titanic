50bd121a0dd64100ca87cadc89b3303dcee88294
kamran Arshad <ikramhashmi1111@gmail.com>
2025-03-10 09:11:58 +0500

    updated

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template,request
+from prediction import prediction
+import pandas as pd
+df=pd.read_csv("artifacts\\merged_titanic.csv")
+
+data=df.drop(["PassengerId","Name","Ticket","Cabin"],axis=1)
+col=data.head(1).columns
+print(len(col))
 
 
 
@@ -7,19 +14,24 @@ app =Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def Home():
     if request.method == 'POST':
-        Pclass=request.form['Pclass']
-        Sex=request.form['Sex']
-        Age=request.form['Age']
-        SibSp=request.form['SibSp']
-        Parch=request.form['Parch']
-        Fare=request.form['Fare']
-        Embarked=request.form['Embarked']
-        total=[Pclass,Sex,Age,SibSp,Parch,Fare,Embarked]
-        
+        Pclass =float (request.form.get("Pclass"))
+        Sex =float (request.form.get("Sex"))
+        Age =float (request.form.get("Age"))
+        SibSp =float (request.form.get("SibSp"))
+        Parch =float (request.form.get("Parch"))
+        Fare =float (request.form.get("Fare"))
+        Embarked =float(request.form.get("Embarked"))
+        total=[[Pclass,Sex,Age,SibSp,Parch,Fare,Embarked]]
+        a = prediction(data=total)
+        if a[0] == 1:
+            a = "Maligant"
+        else:
+            a = "Benign"
 
         
-        return render_template("index.html",name = total)
+        return render_template("index.html",name =a, columns=col)
 
-    return render_template("index.html",name='ikram')    
+    return render_template("index.html",coulmns=col)    
+
 if __name__ == "__main__":
     app.run(debug=True)    
--- prediction.py
+++ prediction.py
@@ -1,16 +1,15 @@
 import joblib as j
 import pandas as pd
+sc=j.load("models\\scaler.pkl")
+model = j.load("models\\model.pkl")
 
-sc=j.load("models/scaler.pkl")
-model = j.load("models/model.pkl")
 
-
-df=pd.read_csv("artifacts/cleaned.csv")
-data=df.drop(["PassengerId","Name","Ticket","Cabin"],inplace=True,axis=1)
-data=data.head(1).values
+df=pd.read_csv("artifacts\\cleaned.csv")
+# data=df.drop(["PassengerId","Name","Ticket","Cabin"],axis=1)
+data=df.head(1).values
 
 def prediction(data=data,sc=sc,model=model):
-    data=sc.transform(data)
+    # data=sc.transform(data)
     prediction=model.predict(data)
     return prediction
 
